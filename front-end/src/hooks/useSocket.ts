c5865c08dc94649e14b05c1fbcf21c117aad7df1
Alfazal007 <alfazal7777@gmail.com>
2024-10-02 23:11:10 +0530

    removed unused prop

--- front-end/src/hooks/useSocket.ts
+++ front-end/src/hooks/useSocket.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const WS_URL = "ws://localhost:8001";
 
 export const useSocket = () => {
-    const [isConnecting, setIsConnecting] = useState(false);
+    const [_, setIsConnecting] = useState(false);
     const [socket, setSocket] = useState<WebSocket | null>(null);
     useEffect(() => {
         setIsConnecting(true);
